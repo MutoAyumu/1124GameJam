7fb860ea0de2a028c74206904df6e83494e7a425
MutoAyumu <mutoayum0610@gmail.com>
2021-11-24 11:57:15 +0900

    Generatorを作った

---
+++ Assets/Muto/Scripts/FishScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class FishScript : MonoBehaviour
+{
+    [SerializeField] int _score = 100;
+    [SerializeField] float _moveSpeed = 1;
+    Rigidbody2D _rb;
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        _rb.gravityScale = 0;
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+    void Move()
+    {
+        _rb.velocity = this.transform.right * Mathf.PerlinNoise(Time.time, 0) * _moveSpeed;
+    }
+}
--- Assets/Muto/Scripts/Generator.cs
+++ Assets/Muto/Scripts/Generator.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class Generator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] GameObject[] _fishs = default;
+    [SerializeField] GameObject _fishGroup = default;
+    [SerializeField, Tooltip("XがMin　YがMax")] Vector2 _instanceInterval = Vector2.zero;
 
-    // Update is called once per frame
-    void Update()
+    float _timer;
+    float _setTime;
+
+    private void Start()
     {
-        
+        _setTime = _instanceInterval.x;
+    }
+    private void Update()
+    {
+        _timer += Time.deltaTime;
+
+        if (_timer > _setTime)
+        {
+            Debug.Log(Mathf.Floor(_setTime));
+            Generate();
+        }
+    }
+    void　Generate()
+    {
+        var fish = Instantiate(_fishs[Random.Range(0, _fishs.Length)], this.transform.position, Quaternion.identity);
+
+        _timer = 0;
+        _setTime = Random.Range(_instanceInterval.x, _instanceInterval.y);
+
+        if (_fishGroup)
+        {
+            fish.transform.SetParent(_fishGroup.transform);
+        }
     }
 }
