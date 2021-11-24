026249bd84402ddc75bd7814088051aaf4138be9
伊東聖矢 <tvedaamamesab@gmail.com>
2021-11-24 15:43:11 +0900

    釣り上げの実装

---
+++ Assets/Ito/Scripts/FishingHook.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishingHook : MonoBehaviour
+{
+    [SerializeField] string _fishTag;
+    private float _fishRotate = 90f;
+    private bool _isHit = false;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == _fishTag)
+        {
+            if (_isHit) return;
+
+            var fish = collision.GetComponent<FishScript>();
+            fish.transform.SetParent(transform);
+            fish.transform.Rotate(0f, 0f, _fishRotate);
+
+            var collider = GetComponent<Collider2D>();
+            collider.enabled = false;
+
+            _isHit = true;
+        }
+
+    }
+}
