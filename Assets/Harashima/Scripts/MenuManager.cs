24271ad2df2b1a4986cae69bf8e866bcc7d27404
HarashimaYoshikazu <harashima318@gmail.com>
2021-11-24 11:53:11 +0900

    ScoreManager,MenuManagerの追加、Prefubの追加

---
+++ Assets/Harashima/Scripts/MenuManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField] GameObject _menuPanel;
+    void Start()
+    {
+
+    }
+    public void SetActiveMenu(bool isOpen)
+    {
+        _menuPanel.SetActive(isOpen);
+    }
+
+    public void PowerUp(float useMoney)
+    {
+        if (ScoreManager.Money < useMoney)
+        {
+            return;
+        }
+        else
+        {
+            ScoreManager.Money -= useMoney;
+        }
+    }
+}
